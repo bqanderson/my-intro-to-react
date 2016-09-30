39423762b21878712e747db3b4e4d3e8351c2d05
Brian Anderson <bqanderson@me.com>
2016-09-30 02:28:54 -0500

    Add Composite Components, Factory, and Props. Plus colors

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,9 +1,25 @@
 var div = React.DOM.div
 var h1 = React.DOM.h1
+var h2 = React.DOM.h2
+
+var MyTitle = React.createClass({
+  render () {
+    return (
+      div(null,
+        h1({style: {color: this.props.color}}, this.props.title)
+      )
+    )
+  }
+})
+
+var MyTitleFact = React.createFactory(MyTitle)
+var ce = React.createElement
 
 var MyFirstComponent = (
   div(null,
-    h1(null, 'Welcome to the wide world of React.js!')
+    MyTitleFact({title: 'Props are super cool!', color: 'rebeccapurple'}),
+    React.createElement(MyTitle, {title: 'I got your Props right here.', color: 'burlywood'}),
+    ce(MyTitle, {title: 'Props are actually super useful!', color: 'steelblue'})
   )
 )
 
