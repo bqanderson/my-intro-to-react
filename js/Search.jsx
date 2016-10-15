b5a17925b13022a8214b77cffcb5e2b74cfd89c6
Brian Anderson <bqanderson@me.com>
2016-10-15 18:28:13 -0500

    Add managed state.

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,13 +2,23 @@ const React = require('react')
 const ShowCard = require('./ShowCard')
 const data = require('../public/data')
 
-class Search extends React.Component {
+const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'This is my search Term'
+    }
+  },
+  handelSearchTermEvent (event) {
+    this.setState({ searchTerm: event.target.value })
+  },
   render () {
     return (
       <div className='container'>
         <header className='header'>
-          <h1 className='brand'>Vido Finder v2.1</h1>
-          <input className='search-input' type='text' placeholder='Search...' />
+          <h1 className='brand'>{this.state.searchTerm}</h1>
+          <input value={this.state.searchTerm} className='search-input' type='text'
+            placeholder='Search...' onChange={this.handelSearchTermEvent}
+          />
         </header>
         <div className='shows'>
           {data.shows.map((show) => (
@@ -18,6 +28,6 @@ class Search extends React.Component {
       </div>
     )
   }
-}
+})
 
 module.exports = Search
